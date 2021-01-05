a69ce0f17ffbc0350cc0b27c7ec33bc17c3dd8c1
alisoltanirad <soltaniradali@gmail.com>
2021-01-05 22:20:34 +0330

    Add shortcut script

---
+++ shortcut.sh
@@ -0,0 +1,21 @@
+"""
+Usage: bash shortcut.sh dirs_path [target]
+
+Args:
+  dirs_path: Path of directories.
+  target (optional): Target path to make shortcuts. Default is home (~/).
+
+"""
+dirs_path=$1
+target=${2:-~/}
+
+cd $target
+for path in $dirs_path*; do
+  if test -e "$target${path##/*/}"
+  then
+    echo "\"${path##/*/}\" exists in \"$target\"."
+  else
+    ln -s $path ${path##/*/}
+    echo "\"${path##/*/}\" created in \"$target\"."
+  fi
+done
